bd0a0d964c2e25c77818b313948d10d746c6ae57
uzmayasmeen <uzma2461@gmail.com>
2026-07-05 19:44:28 +0530

    Show full ORVA app on production landing

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
-import { ComingSoonPage } from "../components/ComingSoonPage";
 import { SimpleHomeLanding } from "../components/SimpleHomeLanding";
 
 export default function Home() {
-  const forceComingSoon = process.env.NEXT_PUBLIC_SHOW_COMING_SOON === "true";
-
-  if (forceComingSoon) {
-    return <ComingSoonPage />;
-  }
-
   return <SimpleHomeLanding />;
 }
